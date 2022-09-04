b0cc4b28cb541589485e22529334792dc213c0ab
bkstud <bkstud@em.com>
2022-09-04 13:45:41 +0200

    add file for api calls

---
+++ src/rest_api/RestApi.jsx
@@ -0,0 +1,20 @@
+const BACKEND_ADDRESS = process.env.REACT_APP_BACKEND_ADDRESS || "https://localhost:3001"
+
+
+const getApiUrl = () => {
+    return BACKEND_ADDRESS + "/api/v1"
+}
+
+async function postTender(tender) {
+    return fetch(getApiUrl() + "/tender/new", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(tender)
+                })
+}
+
+export {
+    postTender,
+}
